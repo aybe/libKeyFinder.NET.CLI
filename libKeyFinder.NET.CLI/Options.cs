e0f1900af4fe27aea959f03ac93456bb674074bd
Aybe <aybe@users.noreply.github.com>
2017-12-21 02:16:42 +0100

    First version

---
+++ libKeyFinder.NET.CLI/Options.cs
@@ -0,0 +1,24 @@
+﻿using CommandLine;
+
+namespace libKeyFinder.NET.CLI
+{
+    internal class Options
+    {
+        public const int DefaultBlockSize = 1024;
+
+        [Option('p', "path",
+            Required = true,
+            SetName = "detect",
+            HelpText = "Path to an audio file"
+        )]
+        public string Path { get; set; }
+
+        [Option('b', "blocksize",
+            Required = false,
+            SetName = "detect",
+            DefaultValue = DefaultBlockSize,
+            HelpText = "Block size in samples"
+        )]
+        public int BlockSize { get; set; }
+    }
+}
--- libKeyFinder.NET.CLI/Program.cs
+++ libKeyFinder.NET.CLI/Program.cs
@@ -1,51 +1,86 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using CommandLine;
 using libsndfile.NET;
 
 namespace libKeyFinder.NET.CLI
 {
-    internal class Program
+    internal static class Program
     {
-        private static void Main(string[] args)
+        private static int Main(string[] args)
         {
-            var path = @"C:\Users\Aybe\Music\Forme - Söndag.wav";
-
-            using (var sf = SndFile.OpenRead(path))
+            var result = Parser.Default.ParseArguments<Options>(args);
+            if (result.Errors.Any()) // prints usage
             {
-                if (sf == null)
-                    throw new InvalidOperationException();
+                Console.WriteLine("Return codes:");
+                Console.WriteLine("  Help         : -2");
+                Console.WriteLine("  Error        : -1");
+                foreach (var k in Enum.GetValues(typeof(Key)).Cast<Key>())
+                    Console.WriteLine($"  {k,-12} : {(int) k}");
 
-                var format = sf.Format;
-                var frames = 16384;
-                var channels = format.Channels;
-                var samples = frames * channels;
-                using (var kd = new KeyDetector(format.SampleRate, channels, samples))
-                {
-                    var buffer = new double[samples];
-                    long read;
-                    long total = 0;
-                    do
-                    {
-                        read = sf.ReadFrames(buffer, frames);
+                Console.WriteLine();
+                Console.WriteLine("Credits :");
+                Console.WriteLine("  https://github.com/ibsh/libKeyFinder");
+                Console.WriteLine("  https://github.com/aybe/libKeyFinder.NET");
+                Console.WriteLine("  https://github.com/erikd/libsndfile");
 
-                        var len1 = read * channels;
-
-                        for (var i = 0; i < len1; i++)
-                            kd.SetSample((uint) i, buffer[i]);
-
-                        for (var i = len1; i < samples; i++)
-                            kd.SetSample((uint) i, 0.0d);
-
-                        kd.ProgressiveChromagram();
-
-                        total += read;
-                        var progress = (double) total / sf.Frames;
-                        Console.WriteLine($"{progress:P} {kd.KeyOfChromagram()}");
-
-                    } while (read == frames);
-
-                    Console.WriteLine(kd.KeyOfChromagram());
-                }
+                return -2;
             }
+
+            var options = result.Value;
+
+            var path = options.Path;
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("File not found");
+                return -1;
+            }
+
+            var sf = SndFile.OpenRead(path);
+            if (sf == null)
+            {
+                Console.WriteLine($"File couldn't be opened: '{SndFile.GetErrorMessage()}'.");
+                return -1;
+            }
+
+            var format = sf.Format;
+            var frames = Math.Min(Options.DefaultBlockSize, options.BlockSize);
+            var samples = frames * format.Channels;
+            var buffer = new double[samples];
+            var totalFrames = 0L;
+            var totalPercent = 0;
+            var kd = new KeyDetector(format.SampleRate, format.Channels, samples);
+
+            while (true)
+            {
+                var readFrames = sf.ReadFrames(buffer, frames);
+                if (readFrames <= 0) break;
+
+                var readSamples = readFrames * format.Channels;
+                for (var i = 0; i < readSamples; i++)
+                    kd.SetSample((uint) i, buffer[i]);
+                for (var i = readSamples; i < samples; i++)
+                    kd.SetSample((uint) i, 0.0d);
+
+                kd.ProgressiveChromagram();
+                totalFrames += readFrames;
+
+                var percent = (int) ((double) totalFrames / sf.Frames * 100.0d);
+                if (percent <= totalPercent) continue;
+                var key1 = kd.KeyOfChromagram();
+                Console.WriteLine($"{percent,6}% : {key1,-12} ({(int) key1})");
+                totalPercent = percent;
+            }
+
+            kd.FinalChromagram();
+            var key2 = kd.KeyOfChromagram();
+            kd.Dispose();
+            sf.Dispose();
+            Console.WriteLine();
+            Console.WriteLine($"Finale  : {key2,-12} ({(int) key2})");
+            return (int) key2;
         }
     }
 }
